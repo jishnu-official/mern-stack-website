d6db2496f9a7fab87e55ac2899e7b8e6786c10fb
Jishnu S <jishnuspal@gmail.com>
2020-08-10 00:07:14 +0530

    Initial Commit

---
+++ src/build/precache-manifest.ad12f34f433e6f01f509b2c566124b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edfdc44d5ae5c7d7bde4",
+    "url": "/static/css/main.8283a2e1.chunk.css"
+  },
+  {
+    "revision": "edfdc44d5ae5c7d7bde4",
+    "url": "/static/js/main.bfceaaaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e876c9400b08ac5b26c8",
+    "url": "/static/js/2.c9720769.chunk.js"
+  },
+  {
+    "revision": "8573125d6f7eebfe4a1cb1cbe917fe85",
+    "url": "/index.html"
+  }
+];
